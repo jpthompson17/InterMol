a454725667858d99d55a5ac066c714316bf6d4c2
Jeff Thompson <jpthompson17@gmail.com>
2015-01-28 19:30:50 -0600

    Add _prefactor to HarmonicBondType.

--- intermol/forces/harmonic_bond_type.py
+++ intermol/forces/harmonic_bond_type.py
@@ -5,20 +5,46 @@ from abstract_bond_type import AbstractBondType
 
 
 class HarmonicBondType(AbstractBondType):
-    __slots__ = ['length', 'k', 'order', 'c']
+    """
+    Bond type with potential given by the function
+      
+      V(r) = self.k * ( r^order - length^order )^2
+
+    where r is the bond distance.
+
+    NOTE 1: A prefactor may be set at initialization or through a setter
+    function. The result of setting the prefactor is that
+   
+      self.k = prefactor * k
+
+    where k is the force constant specified in __init__. An example of where
+    this might be useful is for force fields that specify the harmonic bond
+    potential as V = k/2 * (r - length)^2. Note that the prefactor typcically
+    should not be queried (hence, "_prefactor").
+
+    """
+
+    __slots__ = ['length', 'k', 'order', 'c', '_prefactor']
 
     @accepts_compatible_units(None, None, 
                               length=units.nanometers,
                               k=units.kilojoules_per_mole * units.nanometers ** (-2),
                               order=None,
-                              c=None)
+                              c=None,
+                              prefactor=None)
     def __init__(self, bondingtype1, bondingtype2, 
                  length=0.0 * units.nanometers,
                  k=0.0 * units.kilojoules_per_mole * units.nanometers ** (-2),
-                 order=1, c=False):
+                 order=1, c=False, prefactor=1):
         AbstractBondType.__init__(self, bondingtype1, bondingtype2, order, c)
         self.length = length
-        self.k = k
+        self.k = prefactor * k
+        self._prefactor = prefactor
+
+    @prefactor.setter
+    def prefactor(self, prefactor):
+        self.k *= prefactor / self._prefactor
+        self._prefactor = prefactor
 
 
 class HarmonicBond(HarmonicBondType):
@@ -35,3 +61,6 @@ class HarmonicBond(HarmonicBondType):
                 length=length,
                 k=k,
                 order=order, c=c)
+
+    def canonical(self):
+        return self
